e1d99f300383a2876e1c6a2fab20923917193600
Jory Shilmover <Jorys5116@bigfoot.spokane.edu>
2022-06-16 13:46:54 -0700

    GitPages

---
+++ public/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { render } from "react-dom";
+import { HashRouter } from "react-router-dom";
+
+import store from "./redux/store";
+import { Provider } from "react-redux";
+
+import App from "./App";
+import "./styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+render(
+  <Provider store={store}>
+    <HashRouter base="/">
+      <App />
+    </HashRouter>
+  </Provider>,
+  document.getElementById("app")
+);
